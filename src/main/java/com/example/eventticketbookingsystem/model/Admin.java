5ce4120b2c51894cef87181c1969a6fade4dc90d
Sara <it24100732@my.sliit.lk>
2025-05-13 23:48:57 +0530

    Added a constructor to Admin class

--- src/main/java/com/example/eventticketbookingsystem/model/Admin.java
+++ src/main/java/com/example/eventticketbookingsystem/model/Admin.java
@@ -1,6 +1,13 @@
 package com.example.eventticketbookingsystem.model;
 
-public class Admin {
+public class Admin extends User {
+
+    //Constructor for creating a new admin
+    public Admin(String id, String username, String password, String fullName, String email, String phoneNumber) {
+        // Call the parent constructor
+        super(id, username, password, fullName, email, phoneNumber);
+        setAdmin(true); // Set as admin
+    }
 
     public boolean canManageSystem() {
         return true;
