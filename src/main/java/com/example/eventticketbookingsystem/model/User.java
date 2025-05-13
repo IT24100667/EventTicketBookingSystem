79fc16abeafe63a7bc58eacdbceed3da14630765
Supundi <IT24100746@my.sliit.lk>
2025-05-13 23:36:30 +0530

    Created User class with relevant attributes,constructor,methods,getters & setters

---
+++ src/main/java/com/example/eventticketbookingsystem/model/User.java
@@ -0,0 +1,44 @@
+package com.example.eventticketbookingsystem.model;
+
+
+    public class User extends Person {
+        private String username;    // Username for login
+        private String password;    // Password for login
+        private boolean isAdmin;    // Flag to identify if user is an admin
+
+
+        public User(String id, String username, String password, String fullName, String email, String phoneNumber) {
+            super(id, fullName, email, phoneNumber); // Call parent constructor
+            this.username = username;
+            this.password = password;
+            this.isAdmin = false;   // By default, a user is not an admin
+        }
+
+
+        public String getUsername() { return username; }
+        public void setUsername(String username) { this.username = username; }
+
+        public String getPassword() { return password; }
+        public void setPassword(String password) { this.password = password; }
+
+        public boolean isAdmin() { return isAdmin; }
+        public void setAdmin(boolean admin) { isAdmin = admin; }
+
+
+         // Implementation of abstract method from Person class - demonstrates POLYMORPHISM
+
+        @Override
+        public String getRole() {
+            return isAdmin ? "ADMIN" : "USER";
+        }
+
+
+         // Implementation of abstract method from Person class - demonstrates POLYMORPHISM
+         //Returns a string representation of the user for storage
+
+        @Override
+        public String toFileString() {
+            return id + "," + username + "," + password + "," + fullName + "," + email + "," + phoneNumber + "," + isAdmin;
+        }
+    }
+
