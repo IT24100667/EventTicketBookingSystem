6861d0fc6f51881586336c84cd45e656dfad1e99
Supundi <IT24100746@my.sliit.lk>
2025-05-17 11:27:22 +0530

    Created constructor and added getters and setters into User class

--- src/main/java/com/example/eventticketbookingsystem/model/User.java
+++ src/main/java/com/example/eventticketbookingsystem/model/User.java
@@ -18,14 +18,26 @@ package com.example.eventticketbookingsystem.model;
         }
 
         // Getters and setters
-        public String getUsername() { return username; }
-        public void setUsername(String username) { this.username = username; }
+        public String getUsername() {
+            return username;
+        }
+        public void setUsername(String username) {
+            this.username = username;
+        }
 
-        public String getPassword() { return password; }
-        public void setPassword(String password) { this.password = password; }
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
 
-        public boolean isAdmin() { return isAdmin; }
-        public void setAdmin(boolean admin) { isAdmin = admin; }
+        public boolean isAdmin() {
+            return isAdmin;
+        }
+        public void setAdmin(boolean admin) {
+            isAdmin = admin;
+        }
 
 
 
