63d022dfc0c054f520a2ebe19f6b0184d4d68b4f
Supundi <IT24100746@my.sliit.lk>
2025-05-17 11:23:04 +0530

    Created constructor and added getters and setters into User class

--- src/main/java/com/example/eventticketbookingsystem/model/User.java
+++ src/main/java/com/example/eventticketbookingsystem/model/User.java
@@ -7,4 +7,26 @@ package com.example.eventticketbookingsystem.model;
         private boolean isAdmin;    // Flag to identify if user is an admin
 
 
-       
+
+        //Constructor for creating a new user
+
+        public User(String id, String username, String password, String fullName, String email, String phoneNumber) {
+            super(id, fullName, email, phoneNumber); // Call parent constructor
+            this.username = username;
+            this.password = password;
+            this.isAdmin = false;   // By default, a user is not an admin
+        }
+
+        // Getters and setters
+        public String getUsername() { return username; }
+        public void setUsername(String username) { this.username = username; }
+
+        public String getPassword() { return password; }
+        public void setPassword(String password) { this.password = password; }
+
+        public boolean isAdmin() { return isAdmin; }
+        public void setAdmin(boolean admin) { isAdmin = admin; }
+
+
+
+    }
