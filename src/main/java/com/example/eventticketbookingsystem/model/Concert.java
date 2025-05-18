ef529ba772686f18cb43d825e303b9939dfa5d4b
IT24100667 - Mahdhi S <it24100667@my.sliit.lk>
2025-05-18 19:05:37 +0530

    Created Concert.java child class to Event.java parent class

---
+++ src/main/java/com/example/eventticketbookingsystem/model/Concert.java
@@ -0,0 +1,29 @@
+package com.example.eventticketbookingsystem.model;
+
+import java.util.Date;
+
+public class Concert extends Event{
+
+    private String artist;
+
+    public Concert(String name, String description, String venue, Date date, double price, int capacity, String artist) {
+        super(name, description, venue, date, price, capacity, "Concert");  // give event type parameter as concert
+        this.artist = artist;
+    }
+
+
+    // abstract method implemented
+    @Override
+    public double calculateTicketPrice() {
+        return 0;
+    }
+
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}
