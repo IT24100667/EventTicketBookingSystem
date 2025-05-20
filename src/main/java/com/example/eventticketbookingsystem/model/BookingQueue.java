f7ed8242ddcee9e17262da2e187e49129d2d3ef3
IT24100700 <it24100700@my.sliit.lk>
2025-05-20 20:48:00 +0530

    Implementation the actual queue, using our CustomLinkedList and implementation BookingQueue constructor

--- src/main/java/com/example/eventticketbookingsystem/model/BookingQueue.java
+++ src/main/java/com/example/eventticketbookingsystem/model/BookingQueue.java
@@ -1,5 +1,19 @@
 package com.example.eventticketbookingsystem.model;
 
+    // BookingQueue class demonstrates a simple queue data structure
+    // for processing bookings in First-In-First-Out (FIFO) order
+
 public class BookingQueue {
+
+    // The actual queue, using our CustomLinkedList implementation
+
+    private CustomLinkedList queue;
+
+     // Constructor - creates an empty queue
+
+    public BookingQueue() {
+        queue = new CustomLinkedList();
+    }
+
 
 }
