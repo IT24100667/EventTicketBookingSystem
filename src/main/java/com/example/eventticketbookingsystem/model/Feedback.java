38c85f86b09d523b78a4644891cdfeab491285a4
Ashinsana <it24100668@my.sliit.lk>
2025-05-20 21:11:14 +0530

    i create variables in feedback.java

--- src/main/java/com/example/eventticketbookingsystem/model/Feedback.java
+++ src/main/java/com/example/eventticketbookingsystem/model/Feedback.java
@@ -1,4 +1,13 @@
 package com.example.eventticketbookingsystem.model;
 
+import java.util.Date;
+
 public class Feedback {
+    private String id;
+    private String userId;
+    private String username; // Store username for display purposes
+    private int rating; // 1-5 rating (5 is best)
+    private String comment;
+    private Date createdDate;
+    private Date modifiedDate;
 }
