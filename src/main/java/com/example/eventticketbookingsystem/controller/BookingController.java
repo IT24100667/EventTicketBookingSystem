f8ad5085531a63c577214f5abd2714a49f31ec78
IT24100700 <it24100700@my.sliit.lk>
2025-05-21 06:11:23 +0530

    Implementation private attributes and constructor in BookingController.java

--- src/main/java/com/example/eventticketbookingsystem/controller/BookingController.java
+++ src/main/java/com/example/eventticketbookingsystem/controller/BookingController.java
@@ -1,4 +1,18 @@
 package com.example.eventticketbookingsystem.controller;
 
+import com.example.eventticketbookingsystem.model.BookingQueue;
+import com.example.eventticketbookingsystem.util.BookingFileHandler;
+
 public class BookingController {
+
+    private BookingFileHandler fileHandler;
+    private static BookingQueue bookingQueue = new BookingQueue();  // Shared queue for FIFO processing
+    private EventController eventController;
+
+    public BookingController() {
+        this.fileHandler = new BookingFileHandler();
+        this.eventController = new EventController();
+    }
+
+
 }
