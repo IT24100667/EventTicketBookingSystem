01f23c9bb876612b78f6f32e83dc64e84434ee42
Supundi <IT24100746@my.sliit.lk>
2025-05-28 10:00:52 +0530

    Added comments into MergeSort algorithm

--- src/main/java/com/example/eventticketbookingsystem/algorithm/MergeSort.java
+++ src/main/java/com/example/eventticketbookingsystem/algorithm/MergeSort.java
@@ -10,7 +10,7 @@ public class MergeSort {
 
     public static List<Event> sortByDate(List<Event> events){
 
-        // i create a list copy to avoid making changes to our original list
+        // create a list copy to avoid making changes to our original list
         List<Event> sortedList = new ArrayList<>(events);
 
         // if only 1 element or less there is no need to sort
@@ -18,31 +18,34 @@ public class MergeSort {
             return sortedList;  // Already sorted
         }
 
-        // call merge sort for date
+        // call merge sort for date  //to begin the recursive devide and sort
         mergeSortByDate(sortedList, 0, sortedList.size()-1);  // in the pseudo code i used 1 as starting index but here it is 0
 
         return sortedList;
     }
 
-
+    //this method recursively splits list into halves and sorts each half
     private static void mergeByDate(List<Event> events, int start, int middle, int end){
         // first calculate for n1 and n2
-        // n1 = q - p + 1
-        // n2 = r - q
+        // n1 = q - p + 1 //size of 1 st half of array
+        // n2 = r - q //size of 2nd half of array
         // basically these are sizes of first and second halves of the list so lets use new variable names
 
         int sizeLeft = middle - start + 1;
         int sizeRight = end - middle;
-
-        // create the left and right arrays
+                                                     //catching type mistakes at compile time
+        // create the left and right arrays //<Event> = generic type(stores only Event objects)
         List<Event> leftArray = new ArrayList<>(sizeLeft);
         List<Event> rightArray = new ArrayList<>(sizeRight);
 
-
+        //copy the left half of the list into left array
         for (int i = 0; i < sizeLeft; i++) {
-            leftArray.add(events.get(start + i));
+            //start from 0 and take the values in left array and put them into the code                                                                      //cover the left half
+            leftArray.add(events.get(start + i));//moves one index each time using i,you stop at the middle
         }
+        //copy the right half of the list into right array
         for (int j = 0; j < sizeRight; j++) {
+            //start from (middle+1) and move forward add those items into right array
             rightArray.add(events.get(middle + 1 + j));
         }
 
@@ -55,14 +58,22 @@ public class MergeSort {
         while (i < sizeLeft && j < sizeRight) {
             // Compare event dates - put earlier date first (ascending order)
 //            leftArray.get(i).getDate() <= rightArray.get(j).getDate() this does not work so used compare()
+
+            //leftArray.get(i) = current event from left side   //rightArray.get(j) = current event from right side
+            //compareTo = compares 2 dates in left and right arrays
+            //-1 = if left date is before the right date
+            //0 = left and right dates are equal
+            //1 = left date is after the right date
             if (leftArray.get(i).getDate().compareTo(rightArray.get(j).getDate()) <= 0) {
+                //put the current event from the left list into main list(events) at k position
                 events.set(k, leftArray.get(i));
-                i++;
+                i++; //moves next event in the left array
             } else {
+                //put the current event from right list into main list(events) at k position
                 events.set(k, rightArray.get(j));
-                j++;
+                j++; //moves next event in the right array
             }
-            k++;
+            k++; //moves next position in main list(events) where the next sorted event will be placed
 
         }
 
